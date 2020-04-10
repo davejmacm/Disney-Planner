7162cc506658e897212e8a97426cbe74df447e93
davejmacm <davejmacm@gmail.com>
2020-04-10 01:30:13 +0100

    sorted divs in dayplan for seperate areas

--- src/Components/DayCard.js
+++ src/Components/DayCard.js
@@ -8,17 +8,22 @@ const DayCard = (plan) => {
       <div className='day-date'>
         <h2>{plan.date}</h2>
       </div>
-      <div className='day-day'>
-        <p>{plan.day}</p>
-        <select id='park-choice'>
-          <option value='' selected disabled hidden>
-            Where is the magic today:
-          </option>
-          <option value='magic-kingdom'>Magic Kingdom</option>
-          <option value='epcot'>Epcot</option>
-          <option value='hollywod-studios'>Hollywood Studios</option>
-          <option value='animal-kingdom'>Animal Kingdom</option>
-        </select>
+      <div className='day-details'>
+        <div className='day-day'>
+          <p>{plan.day}</p>
+          <select id='park-choice'>
+            <option value='' selected disabled hidden>
+              Where is the magic today:
+            </option>
+            <option value='magic-kingdom'>Magic Kingdom</option>
+            <option value='epcot'>Epcot</option>
+            <option value='hollywod-studios'>Hollywood Studios</option>
+            <option value='animal-kingdom'>Animal Kingdom</option>
+          </select>
+        </div>
+        <div className='day-tasks'>
+          <p>holding text</p>
+        </div>
       </div>
     </div>
   )
