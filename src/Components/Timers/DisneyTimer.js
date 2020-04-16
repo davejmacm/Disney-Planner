0fb54f938e70511e5c3be3084f66be1a3134d910
davejmacm <davejmacm@gmail.com>
2020-04-16 22:49:10 +0100

    adjusted card widths for mobile widths

--- src/Components/Timers/DisneyTimer.js
+++ src/Components/Timers/DisneyTimer.js
@@ -6,12 +6,6 @@ today.setHours(0)
 const dateDiff = Math.round(
   (disneyDate.getTime() - today.getTime()) / (1000 * 3600 * 24)
 )
-console.log('today:', today)
-console.log('disneyDate:', disneyDate)
-console.log(
-  'calc:',
-  disneyDate.getTime() - today.getTime() / (1000 * 3600 * 24)
-)
 
 const getDate = dateDiff > 0 ? dateDiff : 'Today'
 
